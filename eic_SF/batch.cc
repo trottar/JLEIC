f1177027121354d6f49003c882818f00ac17b6ad
Richard Trotta <trotta@cua.edu>
2019-05-02 12:39:55 -0400

    Binned cross section with values from Tanja's old email

--- eic_SF/batch.cc
+++ eic_SF/batch.cc
@@ -1,5 +1,8 @@
 void batch()
 {
     gROOT->ProcessLine(".L TDISMC_EIC.cpp+");
-    gROOT->ProcessLine("mainx(0.00001,1.,1.0,300.0,103)");
+    // gROOT->ProcessLine("mainx(0.00001,1.,1.0,10000.0,103)");
+    gROOT->ProcessLine("mainx(0.001,0.008,1.0,8.0,103)");
+    // gROOT->ProcessLine("mainx(0.01,0.08,10.0,80.0,103)");
+    // gROOT->ProcessLine("mainx(0.1,0.8,100.0,800.0,103)");
 }
