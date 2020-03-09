6633e868ea58cf90067076b4a009a4d55fadb220
Richard Trotta <trotta@cua.edu>
2020-03-09 11:58:55 -0400

    Changes to g4e that inlcudes meson_structure plugins

--- eic_SF/g4e_files/eicMC_g4e.py
+++ eic_SF/g4e_files/eicMC_g4e.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2020-02-27 11:33:46 trottar"
+# Time-stamp: "2020-03-08 14:34:32 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -12,10 +12,17 @@
 #
 
 from g4epy import Geant4Eic
+from pyjano.jana import Jana
 
-g4e=Geant4Eic(detector='jleic',beamline='erhic')\
-                        .source('../OUTPUTS/TDIS_lund_500k.dat')\
-                        .output('jleic_mesonMC_500k')\
-                        .beam_on(1)\
-                        .vis()\
-                        .run()
+g4e = Geant4Eic(detector='jleic', beamline='erhic')\
+         .source('../OUTPUTS/pi_p_lund.dat')\
+         .output('OUTPUTS/g4e_pi_p')\
+         .beam_on(1)\
+         .vis()\
+         .run()
+
+Jana().plugin('g4e_reader')\
+      .plugin('meson_structure')\
+      .plugin('jana', output='OUTPUTS/jana_pi_p.root')\
+      .source('OUTPUTS/g4e_pi_p.root')\
+      .run()
