fb20b4ee25ab7550336968b881b39c10071acf27
Richard Trotta <trotta@cua.edu>
2021-06-06 15:35:46 -0400

    Added cuts.py for applying cuts. Testing still to be done.

--- USERS/trottar/src/process/bindata.py
+++ USERS/trottar/src/process/bindata.py
@@ -23,26 +23,30 @@ numEvts = len(d.findKey())
 print("The number of events simulated is {}".format(numEvts))
 #print(d.findKey.__doc__)
 
-"""
-Need to figure out how to properly access the 'invts' branch
-"""
-#table = ak.Table(tdict["invts"])
-#print(table)
+
 
 TDIS_xbj_raw = d.findKey('TDIS_xbj')
 fpi_raw = d.findKey('fpi')
 Q2_raw = d.findKey('TDIS_Q2')
 t_raw = d.findKey('TDIS_t')
 xL_raw = d.findKey('xL')
-print("Average (no bin)",np.average(TDIS_xbj_raw))
+y_raw = d.findKey('TDIS_y')
+sigma_dis_raw = d.findKey("sigma_dis")
+f2N_raw = d.findKey("f2N")
+xpi_raw = d.findKey("xpi")
+ypi_raw = d.findKey("ypi")
+tpi_raw = d.findKey("tpi")
 
 xbinwidth = 0.1
 xbins =  np.linspace(0.,1.,1/xbinwidth) 
 qbinwidth = 10
-qbins =  [60,120,240,480]
-#qbins =  np.linspace(0.,1000.,100)
+#qbins =  [60,120,240,480]
+qbins =  np.linspace(0.,1000.,100)
+tbinwidth = 0.1
+tbins =  np.linspace(0.,1.,1/tbinwidth) 
 print("xBj Bins will be", xbins)
 print("Q2 Bins will be", qbins)
+print("t Bins will be", tbins)
 
 def binBool(rawdata,bindata,binwidth):
     booldata = []
@@ -57,6 +61,7 @@ def binBool(rawdata,bindata,binwidth):
 
 xbinVal = np.array(binBool(TDIS_xbj_raw,xbins,xbinwidth))
 qbinVal = np.array(binBool(Q2_raw,qbins,qbinwidth))
+tbinVal = np.array(binBool(t_raw,qbins,tbinwidth))
 
 def binData(lst, binType):
     arr_bin  = np.array(lst)
@@ -68,18 +73,60 @@ Q2_xbin = binData(Q2_raw, xbinVal)
 fpi_xbin = binData(fpi_raw, xbinVal)
 t_xbin = binData(t_raw, xbinVal)
 xL_xbin = binData(xL_raw, xbinVal)
+y_xbin = binData(y_raw, xbinVal)
+sigma_dis_xbin = binData(sigma_dis_raw, xbinVal)*(1e-5) # used to compare to HERA cross-section
+f2N_xbin = binData(f2N_raw, xbinVal)
+xpi_xbin = binData(xpi_raw, xbinVal)
+ypi_xbin = binData(ypi_raw, xbinVal)
+tpi_xbin = binData(tpi_raw, xbinVal)
 
 TDIS_xbj_qbin = binData(TDIS_xbj_xbin, qbinVal)
 Q2_qbin = binData(Q2_xbin, qbinVal)
 fpi_qbin = binData(fpi_xbin, qbinVal)
 t_qbin = binData(t_xbin, qbinVal)
 xL_qbin = binData(xL_xbin, qbinVal)
+y_qbin = binData(y_xbin, qbinVal)
+sigma_dis_qbin = binData(sigma_dis_xbin, qbinVal)
+f2N_qbin = binData(f2N_xbin, qbinVal)
+xpi_qbin = binData(xpi_xbin, qbinVal)
+ypi_qbin = binData(ypi_xbin, qbinVal)
+tpi_qbin = binData(tpi_xbin, qbinVal)
 
 TDIS_xbj_qbin = np.trim_zeros(TDIS_xbj_qbin)
 Q2_qbin = np.trim_zeros(Q2_qbin)
 fpi_qbin = np.trim_zeros(fpi_qbin)
 t_qbin = np.trim_zeros(t_qbin)
 xL_qbin = np.trim_zeros(xL_qbin)
+y_qbin = np.trim_zeros(y_qbin)
+sigma_dis_qbin = np.trim_zeros(sigma_dis_qbin)
+f2N_qbin = np.trim_zeros(f2N_qbin)
+xpi_qbin = np.trim_zeros(xpi_qbin)
+ypi_qbin = np.trim_zeros(ypi_qbin)
+tpi_qbin = np.trim_zeros(tpi_qbin)
+
+TDIS_xbj_tbin = binData(TDIS_xbj_xbin, tbinVal)
+Q2_tbin = binData(Q2_xbin, tbinVal)
+fpi_tbin = binData(fpi_xbin, tbinVal)
+t_tbin = binData(t_xbin, tbinVal)
+xL_tbin = binData(xL_xbin, tbinVal)
+y_tbin = binData(y_xbin, tbinVal)
+sigma_dis_tbin = binData(sigma_dis_xbin, tbinVal)
+f2N_tbin = binData(f2N_xbin, tbinVal)
+xpi_tbin = binData(xpi_xbin, tbinVal)
+ypi_tbin = binData(ypi_xbin, tbinVal)
+tpi_tbin = binData(tpi_xbin, tbinVal)
+
+TDIS_xbj_tbin = np.trim_zeros(TDIS_xbj_tbin)
+Q2_tbin = np.trim_zeros(Q2_tbin)
+fpi_tbin = np.trim_zeros(fpi_tbin)
+t_tbin = np.trim_zeros(t_tbin)
+xL_tbin = np.trim_zeros(xL_tbin)
+y_tbin = np.trim_zeros(y_tbin)
+sigma_dis_tbin = np.trim_zeros(sigma_dis_tbin)
+f2N_tbin = np.trim_zeros(f2N_tbin)
+xpi_tbin = np.trim_zeros(xpi_tbin)
+ypi_tbin = np.trim_zeros(ypi_tbin)
+tpi_tbin = np.trim_zeros(tpi_tbin)
 
 '''
 Next step is to restructure MC root files then bin everything
--- USERS/trottar/src/process/plot_test.py
+++ USERS/trottar/src/process/plot_test.py
@@ -11,8 +11,15 @@
 # Copyright (c) trottar
 #
 
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
+from matplotlib.ticker import MaxNLocator
+
+import sys
+from sys import path
+sys.path.insert(0,'./cuts/')
+import cuts as c
 
 import bindata as data
 
@@ -37,6 +44,127 @@ def densityPlot(x,y,title,xlabel,ylabel,binx,biny,
         
     return fig
 
+# Create cut dictionary
+cutDict = {}
+
+'''
+xbj = data.TDIS_xbj_raw
+Q2 = data.Q2_raw
+y = data.y_raw
+t = data.t_raw
+fpi = data.fpi_raw
+xpi = data.TDIS_xbj_raw/(1-data.xL_raw)
+
+xbj = data.TDIS_xbj_xbin
+Q2 = data.Q2_xbin
+y = data.y_xbin
+t = data.t_xbin
+fpi = data.fpi_xbin
+xpi = data.TDIS_xbj_xbin/(1-data.xL_xbin)
+
+xbj = data.TDIS_xbj_qbin
+Q2 = data.Q2_qbin
+y = data.y_qbin
+t = data.t_qbin
+fpi = data.fpi_qbin
+xpi = data.TDIS_xbj_qbin/(1-data.xL_qbin)
+'''
+xbj = data.TDIS_xbj_tbin
+Q2 = data.Q2_tbin
+y = data.y_tbin
+t = data.t_tbin
+fpi = data.fpi_tbin
+xpi = data.TDIS_xbj_tbin/(1-data.xL_tbin)
+
+Q2binarray = [7,15,30,60,120,240,480,1000]
+for i,x in enumerate(Q2binarray) :
+    Q2tmp = '{"Q2cut%i" : ((%0.1f <= Q2) & (Q2 <= %0.1f))}' % (i,Q2binarray[i]-data.qbinwidth/20,Q2binarray[i]+data.qbinwidth/20)
+    print('{"Q2cut%i" : ((%0.1f <= Q2) & (Q2 <= %0.1f))}' % (i,Q2binarray[i]-data.qbinwidth/20,Q2binarray[i]+data.qbinwidth/20))
+    cutDict.update(eval(Q2tmp))
+
+# xarray = np.arange(0.05,1.0,0.1).tolist() # table, large x (usual)
+xarray = np.arange(data.xbinwidth/2,1.0,data.xbinwidth).tolist() # table, small x 
+for i,x in enumerate(xarray):
+    xtmp = '{"xcut%i" : ((%0.4f <= xpi) & (xpi <= %0.4f))}' % (i,xarray[i]-data.xbinwidth/2,xarray[i]+data.xbinwidth/2)
+    print('{"xcut%i" : ((%0.4f <= xpi) & (xpi <= %0.4f))}' % (i,xarray[i]-data.xbinwidth/2,xarray[i]+data.xbinwidth/2))
+    cutDict.update(eval(xtmp))
+
+ytmp = '{"ycut" : ((0.01 <= y) & (y <= 0.95))}'
+ttmp = '{"tcut" : ((-1.00 <= t) & (t <= 0.00))}'
+cutDict.update(eval(ttmp))
+cutDict.update(eval(ytmp))
+cut = c.pyPlot(cutDict)    
+
+ycut1 = ["ycut"]
+tcut1 = ["tcut"]
+#cut_t_y = ["xcut0","Q2cut3","tcut","ycut"] # Q2= 60 GeV^2
+cut_t_y = ["xcut0","tcut","ycut"] # Q2= 60 GeV^2
+
+def fpivxpi_Plot():
+    
+    f = plt.figure(figsize=(11.69,8.27))
+    plt.rcParams.update({'font.size': 15})
+    plt.style.use('classic')
+    
+    ax = f.add_subplot(221)
+    xpiscat4 = ax.scatter(cut.applyCuts(xpi,cut_t_y),cut.applyCuts(fpi,cut_t_y),label='$Q^2$=60 $GeV^2$')
+    #xpiscat4a = ax.scatter(xpi,fpi,label='$Q^2$=60 $GeV^2$')
+    plt.plot([0.001,0.01,0.1],[1.2,0.45,0.25], label="GRV fit",color="y")
+    plt.xscale('log')
+    plt.ylim(-0.1,0.3)
+    plt.xlim(1e-3,1.)
+    ax.text(0.25, 0.65, '$Q^2$=60 $GeV^2$', transform=ax.transAxes, fontsize=15, verticalalignment='top', horizontalalignment='left')
+    ax.yaxis.set_major_locator(MaxNLocator(prune='both'))
+    ax.xaxis.set_major_formatter(plt.NullFormatter())
+    ax.set_yticks([-0.3,-0.2,-0.1,0.0,0.1,0.2,0.3])
+    ax.set_xticks([1e-2,1e-1])
+    
+    plt.ylabel('$F^{\pi}_{2}$', fontsize=20)
+    
+    '''
+    ax = f.add_subplot(222)
+    xpiscat5 = ax.errorbar(cut.applyCuts(xpi,cut120),cut.applyCuts(fpi,cut120),yerr=np.sqrt(cut.applyCuts(lumi,cut120))/cut.applyCuts(lumi,cut120),fmt='.',label='$Q^2$=120 $GeV^2$',ecolor='cyan',capsize=2, capthick=2)
+    plt.plot([0.01,0.1,0.3],[0.5,0.25,0.15], label="GRV fit",color="y")
+    plt.xscale('log')
+    plt.ylim(-0.1,0.3)
+    plt.xlim(1e-3,1.)
+    ax.text(0.25, 0.65, '$Q^2$=120 $GeV^2$', transform=ax.transAxes, fontsize=15, verticalalignment='top', horizontalalignment='left')
+    ax.yaxis.set_major_formatter(plt.NullFormatter())
+    ax.xaxis.set_major_formatter(plt.NullFormatter())
+    ax.set_yticks([-0.3,-0.2,-0.1,0.0,0.1,0.2,0.3])
+    ax.set_xticks([1e-2,1e-1])
+    
+    ax = f.add_subplot(223)
+    xpiscat6 = ax.errorbar(cut.applyCuts(xpi,cut240),cut.applyCuts(fpi,cut240),yerr=np.sqrt(cut.applyCuts(lumi,cut240))/cut.applyCuts(lumi,cut240),fmt='.',label='$Q^2$=240 $GeV^2$',ecolor='cyan',capsize=2, capthick=2)
+    plt.plot([0.01,0.1,0.3],[0.55,0.25,0.15], label="GRV fit",color="y")
+    plt.xscale('log')
+    plt.ylim(-0.1,0.3)
+    plt.xlim(1e-3,1.)
+    ax.text(0.25, 0.65, '$Q^2$=240 $GeV^2$', transform=ax.transAxes, fontsize=15, verticalalignment='top', horizontalalignment='left')
+    ax.yaxis.set_major_locator(MaxNLocator(prune='both'))
+    ax.set_yticks([-0.3,-0.2,-0.1,0.0,0.1,0.2,0.3])
+    ax.set_xticks([1e-2,1e-1])
+
+    ax = f.add_subplot(224)
+    xpiscat7 = ax.errorbar(cut.applyCuts(xpi,cut480),cut.applyCuts(fpi,cut480),yerr=np.sqrt(cut.applyCuts(lumi,cut480))/cut.applyCuts(lumi,cut480),fmt='.',label='$Q^2$=480 $GeV^2$',ecolor='cyan',capsize=2, capthick=2)
+    plt.plot([0.01,0.1,0.3],[0.55,0.25,0.15], label="GRV fit",color="y")
+    plt.xscale('log')
+    plt.ylim(-0.1,0.3)
+    plt.xlim(1e-3,1.)
+    ax.text(0.25, 0.65, '$Q^2$=480 $GeV^2$', transform=ax.transAxes, fontsize=15, verticalalignment='top', horizontalalignment='left')
+    ax.yaxis.set_major_formatter(plt.NullFormatter())
+    ax.set_yticks([-0.3,-0.2,-0.1,0.0,0.1,0.2,0.3])
+    ax.set_xticks([1e-2,1e-1])
+    '''
+
+    plt.xlabel('$x_\pi$', fontsize=20)    
+    plt.tight_layout()
+    plt.subplots_adjust(hspace=0.0,wspace=0.0)
+
+    plt.style.use('default')
+
+fpivxpi_Plot()
+
 fig = plt.figure(figsize=(17,12),facecolor='silver')
 
 ax = fig.add_subplot(331)
@@ -44,23 +172,31 @@ ax = fig.add_subplot(331)
 densityPlot(data.t_qbin,data.fpi_qbin, '$fpi$ vs $t$','$t$','$fpi$', 200, 200, ax=ax, fig=fig)
 
 ax = fig.add_subplot(332)
+#plt.scatter(TDIS_xbj_xbin,fpi_xbin)
+densityPlot(data.TDIS_xbj_xbin,data.fpi_xbin, '$fpi$ vs $x$','$x$','$fpi$', 200, 200, ax=ax, fig=fig)
+
+ax = fig.add_subplot(333)
 #plt.scatter(TDIS_xbj_qbin,fpi_qbin)
 densityPlot(data.TDIS_xbj_qbin,data.fpi_qbin, '$fpi$ vs $x$','$x$','$fpi$', 200, 200, ax=ax, fig=fig)
 
-ax = fig.add_subplot(333)
+ax = fig.add_subplot(334)
 densityPlot(data.TDIS_xbj_qbin,data.xL_qbin, '$xL$ vs $x$','$x$','$xL$', 200, 200, ax=ax, fig=fig)
 
-ax = fig.add_subplot(334)
-densityPlot(data.TDIS_xbj_qbin,data.t_qbin, '$t$ vs $x$','$x$','$t$', 200, 200, ax=ax, fig=fig)
-
 ax = fig.add_subplot(335)
-densityPlot(data.xL_qbin,data.t_qbin, '$t$ vs $xL$','$xL$','$t$', 200, 200, ax=ax, fig=fig)
+densityPlot(xbj,t, '$t$ vs $x$','$x$','$t$', 200, 200, ax=ax, fig=fig)
 
 ax = fig.add_subplot(336)
-densityPlot(data.TDIS_xbj_xbin,data.Q2_xbin, '$Q^2$ vs $x$','$x$','$Q^{2}$', 200, 200, ax=ax, fig=fig)
+densityPlot(data.xL_qbin,data.t_qbin, '$t$ vs $xL$','$xL$','$t$', 200, 200, ax=ax, fig=fig)
 
 ax = fig.add_subplot(337)
+densityPlot(data.TDIS_xbj_xbin,data.Q2_xbin, '$Q^2$ vs $x$','$x$','$Q^{2}$', 200, 200, ax=ax, fig=fig)
+
+ax = fig.add_subplot(338)
 densityPlot(data.TDIS_xbj_qbin,data.Q2_qbin, '$Q^2$ vs $x$','$x$','$Q^{2}$', 200, 200, ax=ax, fig=fig)
+
+ax = fig.add_subplot(339)
+densityPlot(cut.applyCuts(xbj,cut_t_y),cut.applyCuts(Q2,cut_t_y), '$Q^2$ vs $x$','$x$','$Q^{2}$', 200, 200, ax=ax, fig=fig)
+#plt.scatter(cut.applyCuts(xbj,cut_t_y),cut.applyCuts(Q2,cut_t_y))
 
 plt.tight_layout()
 plt.show()
